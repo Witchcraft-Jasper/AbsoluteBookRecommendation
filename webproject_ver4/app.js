63556eef9bb7b30595e256d22a83580b441869c6
Witchcraft-Jasper <dszlym666@gmail.com>
2022-07-08 19:55:33 +0800

    前端代码更新

--- webproject_ver4/app.js
+++ webproject_ver4/app.js
@@ -1,7 +1,12 @@
 let express = require("express");
 let path = require("path");
 let cors = require('cors')
+let request = require('request');
 let app = express();
+let bodyParser = require('body-parser');//用于req.body获取值的
+app.use(bodyParser.json());
+// 创建 application/x-www-form-urlencoded 编码解析
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // set static
 app.use(cors());
@@ -18,6 +23,24 @@ app.use("/index.html", indexRouter);
 app.use("/login.html", loginRouter);
 app.use("/signUp.html", signUpRouter);
 app.use("/description.html", descRouter);
+
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    // 2.允许客户端使用哪些请求方法访问我
+    res.header('Access-Control-Allow-Methods', 'PUT,GET,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', ['mytoken', 'Content-Type']);
+    next();
+})
+
+app.get('/hotBooks', (req, res) => {
+    request({ url: 'http://39.106.137.16:3180/api/hotBooks' }, function (error, response, body) {
+        //    console.log(response)
+        //把请求成功后的数据发送给客户端
+        console.log(body);
+        res.send(body);
+    })
+});
 
 app.listen(3000, () => {
     console.log('Server has listen to 3000');
--- webproject_ver4/static/js/loadData.js
+++ webproject_ver4/static/js/loadData.js
@@ -24,24 +24,29 @@ $.ajax({
     }
 });
 
+var content = []
 
 //获取当前书籍
 $.ajax({
     type: "get",
-    url: "../js/data.json",
+    url: "./hotBooks",
     async: true,
-    success: function (data) {
-        console.log(data.toString());
-        for (let i = 6; i < 56; i++) {
-            const str2 = ".book" + (i + 1).toString();
+    timeout: 3000,
+    success: function (obj) {
+        let body = $.parseJSON(obj)
+        console.log(body.toString());
+        content = obj.data
+        for (let i = 0; i < 50; i++) {
+            const str2 = ".book" + (i + 6).toString();
             const books2 = $(str2).children('.product__content');
-            books2.children('h4').children('a').text(str.books[i % 7].name)
-            books2.children('.prize').children('li').text(str.books[i % 7].price);
-            books2.children('.prize').children('.old_prize').text(str.books[i % 7].oldPrice);
-            $(str2).children('.product__thumb').children('.first__img').children('img').attr("src", str.books[i % 7].url);
+            books2.children('h4').children('a').text(content[i].bookName)
+            books2.children('.prize').children('li').text(content[i].price);
+            books2.children('.prize').children('.old_prize').text(content[i].oriPrice);
+            $(str2).children('.product__thumb').children('.first__img').children('img').attr("src", content[i].image);
         }
     }
 });
+
 
 
 function showBooksByCategory(kind) {
